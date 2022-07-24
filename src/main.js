b5a29805275bc30ca04318044d9ad86c6c1a2df2
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-24 17:07:42 +0700

    fix: top-level await

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-console.log(`downloading`);
-console.time('fetch')
-const response = await fetch('/khmer-dict.db')
-const arrayBuffer = await response.arrayBuffer();
-const fileSize = arrayBuffer.byteLength / 1024 / 1024;
-console.timeEnd('fetch')
-console.log(`file size = ${fileSize}mb`);
+
+async function main() {
+  console.log(`downloading`);
+  console.time('fetch')
+  const response = await fetch('/khmer-dict.db')
+  const arrayBuffer = await response.arrayBuffer();
+  const fileSize = arrayBuffer.byteLength / 1024 / 1024;
+  console.timeEnd('fetch')
+  console.log(`file size = ${fileSize}mb`);
+}
+
+
+main()
