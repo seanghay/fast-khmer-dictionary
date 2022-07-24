d5a53f7ee063ea59a2e38e443e6cd7383825c875
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-24 17:22:29 +0700

    fix: sqlite js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import initSqlite from 'sql.js'
 
 async function main() {
   console.log(`downloading`);
@@ -7,7 +8,15 @@ async function main() {
   const fileSize = arrayBuffer.byteLength / 1024 / 1024;
   console.timeEnd('fetch')
   console.log(`file size = ${fileSize}mb`);
-}
 
+  const SQL = await initSqlite({
+    locateFile: file => `https://sql.js.org/dist/${file}`
+  })
+
+  const db = new SQL.Database(new Uint8Array(arrayBuffer));
+  const result = await db.exec('SELECT * FROM dict LIMIT 10');
+  console.log(result);
+
+}
 
 main()
