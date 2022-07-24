9f83247654fabd67309c489f2832b7aa6c4479f5
Seanghay Yath <seanghay.dev@gmail.com>
2022-07-24 17:04:28 +0700

    chore: add vite

---
+++ src/main.js
@@ -0,0 +1,7 @@
+console.log(`downloading`);
+console.time('fetch')
+const response = await fetch('/khmer-dict.db')
+const arrayBuffer = await response.arrayBuffer();
+const fileSize = arrayBuffer.byteLength / 1024 / 1024;
+console.timeEnd('fetch')
+console.log(`file size = ${fileSize}mb`);
